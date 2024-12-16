e71b15566063f4527115b57a5c12ba5eb145087d
r.kawano <r.kawano@genbasupport.com>
2024-12-16 15:40:22 +0900

    Journal作成時のアップロード画像からExifを取得できるように修正

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -21,6 +21,20 @@ class JournalsController < ApplicationController
   def create
     @journal = Journal.new(journal_params)
     if @journal.save
+      @journal.images.each do |image|
+        exif = extract_exif_from_image(image)
+        next unless exif
+
+        latitude = exif.gpslatitude
+        longitude = exif.gpslongitude
+        if latitude && longitude
+          image.latitude = convert_to_decimal(latitude)
+          image.longitude = convert_to_decimal(longitude)
+        end
+        date_of_shooting = exif.date_time_original || exif.create_date
+        image.date_of_shooting = date_of_shooting.in_time_zone('Asia/Tokyo') if date_of_shooting
+        image.save
+      end
       redirect_to @journal, notice: 'Journal and Image were successfully created.'
     else
       render :new
@@ -48,5 +62,32 @@ class JournalsController < ApplicationController
 
   def journal_params
     params.require(:journal).permit(:journal_name, :description, images_attributes: [:file])
+  end
+
+  def extract_exif_from_image(image)
+    file = image.file.download
+
+    tmp_file = Tempfile.new(['exif', '.jpg'])
+    tmp_file.binmode
+    tmp_file.write(file)
+    tmp_file.rewind
+
+    exif = MiniExiftool.new(tmp_file.path)
+    tmp_file.close
+
+    exif
+  end
+
+  def convert_to_decimal(degree_string)
+    parts = degree_string.split
+    degrees = parts[0].to_i
+    minutes = parts[2].delete("'").to_i
+    seconds = parts[3].delete('"').to_f
+    direction = parts[4]
+
+    decimal = degrees + (minutes / 60.0) + (seconds / 3600.0)
+    decimal = -decimal if %w[S W].include?(direction)
+
+    decimal.round(4)
   end
 end
