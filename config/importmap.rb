cd78b39ebe7911b24459e98f1477f65bd5a3b71f
r.kawano <r.kawano@genbasupport.com>
2024-12-17 13:34:55 +0900

    JavaScriptのコードを置く場所を修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+# pin_all_from 'app/javascript/images', under: 'images'
+pin "images/show_map", to: "images/show_map.js"
+pin "images/edit_map", to: "images/edit_map.js"
