245eb8634bfc03c1f80a4bb6758ad27a61963faa
r.kawano <r.kawano@genbasupport.com>
2025-02-09 09:02:29 +0900

    軽微なバグおよびCSSを修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link favicon.ico
+//= link logo_full.png
+//= link logo_icon.png
+
